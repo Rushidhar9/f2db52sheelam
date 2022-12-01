7fc2feaefc4e349e20222e393bb553ee608b3086
Rushidhar9 <111991958+Rushidhar9@users.noreply.github.com>
2022-11-30 21:27:04 -0600

    buttons added create delete detail

--- models/biryani.js
+++ models/biryani.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose")
 const biryaniSchema = mongoose.Schema({
-    biryaniType: String,
+    biryaniType: {
+        type : String,
+        maxLength: 10
+    },
     biryaniFlavor: String,
     biryaniPrice: Number
 })
+
+
 module.exports = mongoose.model("Biryani",biryaniSchema)
