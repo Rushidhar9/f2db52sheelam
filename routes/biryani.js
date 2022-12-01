5d52b42ba277812e2a262bd42514a19b72c40a37
Rushidhar9 <111991958+Rushidhar9@users.noreply.github.com>
2022-11-30 21:12:41 -0600

    secured to update

--- routes/biryani.js
+++ routes/biryani.js
@@ -1,6 +1,17 @@
 var express = require('express');
 const biryani_controlers = require('../controllers/biryani');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
+
 /* GET costumes */
 router.get('/', biryani_controlers.biryani_view_all_Page );
 
@@ -9,7 +20,7 @@ router.get('/detail', biryani_controlers.biryani_view_one_Page);
 
 router.get('/create', biryani_controlers.biryani_create_Page);
 
-router.get('/update', biryani_controlers.biryani_update_Page);
+router.get('/update',secured, biryani_controlers.biryani_update_Page);
 
 router.get('/delete', biryani_controlers.biryani_delete_Page);
 
